cf73ef6f345f89fd86393375394cb946c4c9e1aa
Alexander <alex-xp@list.ru>
2023-10-12 16:10:44 +0500

    12.10.2023 16:13

--- 008_PROJECTS_NINJA/.template_project/cmake_config/boost.cmake
+++ 008_PROJECTS_NINJA/.template_project/cmake_config/boost.cmake
@@ -1,97 +1,63 @@
 ##########################################################################################################################################
-#
-# BOOST - немного тяжеловат в сборке, по причине того, что напрямую не использует CMake. 
-# А своя система сборки, очень долго собирает универсальную машину с размером более 1Гб, что крайне редко требуется для проектов. 
-# Дополнительно ко всему - использует статическую сборку (не требует дополнительных библиотек).
-# BOOST лучше получить готовым.
-#
-# WINDOWS - BOOST Устанавливается с компилятором MSYS2 MINGW.
-# pacman -S mingw64/mingw-w64-x86_64-boost
-#
-# LINUX - используется зависимость из репозитория.
-#
-#
-# ВНИМАНИЕ!!! - ПОДКЛЮЧАТЬ БИБЛИОТЕКИ ТОЛЬКО ПО НЕОБХОДИМОСТИ !!!! (увеличивает размер конечного файла непропорцианально требованиям)
-#
-##########################################################################################################################################
+# ПОИСК BOOST из системного компилятора (WINDOWS GCC MSYS2 ; LINUX GCC OS)
+
+set(PROJ_BOOST_COMPONENTS)
+
+# set(PROJ_BOOST_COMPONENTS ${PROJ_BOOST_COMPONENTS} boost_atomic)
+# set(PROJ_BOOST_COMPONENTS ${PROJ_BOOST_COMPONENTS} boost_chrono)
+# set(PROJ_BOOST_COMPONENTS ${PROJ_BOOST_COMPONENTS} boost_container)
+# set(PROJ_BOOST_COMPONENTS ${PROJ_BOOST_COMPONENTS} boost_context)
+# set(PROJ_BOOST_COMPONENTS ${PROJ_BOOST_COMPONENTS} boost_contract)
+# set(PROJ_BOOST_COMPONENTS ${PROJ_BOOST_COMPONENTS} boost_coroutine)
+# set(PROJ_BOOST_COMPONENTS ${PROJ_BOOST_COMPONENTS} boost_date_time)
+# set(PROJ_BOOST_COMPONENTS ${PROJ_BOOST_COMPONENTS} boost_exception)
+# set(PROJ_BOOST_COMPONENTS ${PROJ_BOOST_COMPONENTS} boost_fiber-mt)
+# set(PROJ_BOOST_COMPONENTS ${PROJ_BOOST_COMPONENTS} boost_filesystem)
+# set(PROJ_BOOST_COMPONENTS ${PROJ_BOOST_COMPONENTS} boost_graph_parallel)
+# set(PROJ_BOOST_COMPONENTS ${PROJ_BOOST_COMPONENTS} boost_graph)
+# set(PROJ_BOOST_COMPONENTS ${PROJ_BOOST_COMPONENTS} boost_headers)
+# set(PROJ_BOOST_COMPONENTS ${PROJ_BOOST_COMPONENTS} boost_iostreams)
+# set(PROJ_BOOST_COMPONENTS ${PROJ_BOOST_COMPONENTS} boost_json)
+# set(PROJ_BOOST_COMPONENTS ${PROJ_BOOST_COMPONENTS} boost_locale)
+# set(PROJ_BOOST_COMPONENTS ${PROJ_BOOST_COMPONENTS} boost_log_setup)
+# set(PROJ_BOOST_COMPONENTS ${PROJ_BOOST_COMPONENTS} boost_log)
+# set(PROJ_BOOST_COMPONENTS ${PROJ_BOOST_COMPONENTS} boost_math_c99)
+# set(PROJ_BOOST_COMPONENTS ${PROJ_BOOST_COMPONENTS} boost_math_c99f)
+# set(PROJ_BOOST_COMPONENTS ${PROJ_BOOST_COMPONENTS} boost_math_c99l)
+# set(PROJ_BOOST_COMPONENTS ${PROJ_BOOST_COMPONENTS} boost_math_tr1)
+# set(PROJ_BOOST_COMPONENTS ${PROJ_BOOST_COMPONENTS} boost_math_tr1f)
+# set(PROJ_BOOST_COMPONENTS ${PROJ_BOOST_COMPONENTS} boost_math_tr1l)
+# set(PROJ_BOOST_COMPONENTS ${PROJ_BOOST_COMPONENTS} boost_mpi)
+# set(PROJ_BOOST_COMPONENTS ${PROJ_BOOST_COMPONENTS} boost_nowide)
+# set(PROJ_BOOST_COMPONENTS ${PROJ_BOOST_COMPONENTS} boost_numpy)
+# set(PROJ_BOOST_COMPONENTS ${PROJ_BOOST_COMPONENTS} boost_prg_exec_monitor)
+# set(PROJ_BOOST_COMPONENTS ${PROJ_BOOST_COMPONENTS} boost_program_options)
+# set(PROJ_BOOST_COMPONENTS ${PROJ_BOOST_COMPONENTS} boost_python)
+# set(PROJ_BOOST_COMPONENTS ${PROJ_BOOST_COMPONENTS} boost_random)
+# set(PROJ_BOOST_COMPONENTS ${PROJ_BOOST_COMPONENTS} boost_regex)
+# set(PROJ_BOOST_COMPONENTS ${PROJ_BOOST_COMPONENTS} boost_serialization)
+# set(PROJ_BOOST_COMPONENTS ${PROJ_BOOST_COMPONENTS} boost_stacktrace_noop)
+# set(PROJ_BOOST_COMPONENTS ${PROJ_BOOST_COMPONENTS} boost_stacktrace_windbg_cached)
+# set(PROJ_BOOST_COMPONENTS ${PROJ_BOOST_COMPONENTS} boost_stacktrace_windbg)
+# set(PROJ_BOOST_COMPONENTS ${PROJ_BOOST_COMPONENTS} boost_system)
+# set(PROJ_BOOST_COMPONENTS ${PROJ_BOOST_COMPONENTS} boost_test_exec_monitor)
+# set(PROJ_BOOST_COMPONENTS ${PROJ_BOOST_COMPONENTS} boost_thread)
+# set(PROJ_BOOST_COMPONENTS ${PROJ_BOOST_COMPONENTS} boost_timer)
+# set(PROJ_BOOST_COMPONENTS ${PROJ_BOOST_COMPONENTS} boost_type_erasure)
+# set(PROJ_BOOST_COMPONENTS ${PROJ_BOOST_COMPONENTS} boost_unit_test_framework)
+# set(PROJ_BOOST_COMPONENTS ${PROJ_BOOST_COMPONENTS} boost_url)
+# set(PROJ_BOOST_COMPONENTS ${PROJ_BOOST_COMPONENTS} boost_wave)
+# set(PROJ_BOOST_COMPONENTS ${PROJ_BOOST_COMPONENTS} boost_wserialization)
+
+set(PROJ_BOOST_COMPONENTS ${PROJ_BOOST_COMPONENTS} ALL)
+
+find_package(Boost REQUIRED ${PROJ_BOOST_COMPONENTS})
 
 
 
 ##########################################################################################################################################
-# ДИРЕКТОРИИ ПОИСКА ФАЙЛОВ
-set(INC_DIRS ${INC_DIRS})
+# ДОПОЛНИТЕЛЬНЫЕ КОМПОНЕНТЫ
 if(${WIN32}) # <------------------ WINDOWS --------------------------------------------------------------------------
-    if(${_DEBUG})
-        # DEBUG OPTIONS
-    else()
-        # RELEASE OPTIONS
-    endif()
-else() # <------------------ LINUX -----------------------------------------------------------------------------------
-    if(${_DEBUG})
-        # DEBUG OPTIONS
-    else()
-        # RELEASE OPTIONS
-    endif()
-endif()
-
-
-
-##########################################################################################################################################
-# ДИРЕКТОРИИ ПОИСКА БИБЛИОТЕК
-set(LIB_DIRS ${LIB_DIRS})
-if(${WIN32}) # <------------------ WINDOWS --------------------------------------------------------------------------
-    if(${_DEBUG})
-        # DEBUG OPTIONS
-    else()
-        # RELEASE OPTIONS
-    endif()
-else() # <------------------ LINUX -----------------------------------------------------------------------------------
-    if(${_DEBUG})
-        # DEBUG OPTIONS
-    else()
-        # RELEASE OPTIONS
-    endif()
-endif()
-
-
-##########################################################################################################################################
-# ИСПОЛЬЗУЕМЫЕ БИБЛИОТЕКИ (подключить или отключить через комментарии)
-if(${WIN32}) # <------------------ WINDOWS --------------------------------------------------------------------------
-
-    # set(LIBS ${LIBS} boost_atomic-mt)
-    # set(LIBS ${LIBS} boost_chrono-mt)
-    # set(LIBS ${LIBS} boost_container-mt)
-    # set(LIBS ${LIBS} boost_context-mt)
-    # set(LIBS ${LIBS} boost_contract-mt)
-    # set(LIBS ${LIBS} boost_coroutine-mt)
-    # set(LIBS ${LIBS} boost_date_time-mt)
-    # set(LIBS ${LIBS} boost_exception-mt)
-    # set(LIBS ${LIBS} boost_fiber-mt)
-    # set(LIBS ${LIBS} boost_filesystem-mt)
-    # set(LIBS ${LIBS} boost_graph-mt)
-    # set(LIBS ${LIBS} boost_iostreams-mt)
-    # set(LIBS ${LIBS} boost_json-mt)
-    # set(LIBS ${LIBS} boost_locale-mt)
-    # set(LIBS ${LIBS} boost_log_setup-mt)
-    # set(LIBS ${LIBS} boost_log-mt)
-    # set(LIBS ${LIBS} boost_nowide-mt)
-    # set(LIBS ${LIBS} boost_prg_exec_monitor-mt)
-    # set(LIBS ${LIBS} boost_program_options-mt)
-    # set(LIBS ${LIBS} boost_random-mt)
-    # set(LIBS ${LIBS} boost_regex-mt)
-    # set(LIBS ${LIBS} boost_serialization-mt)
-    # set(LIBS ${LIBS} boost_stacktrace_noop-mt)
-    # set(LIBS ${LIBS} boost_stacktrace_windbg_cached-mt)
-    # set(LIBS ${LIBS} boost_stacktrace_windbg-mt)
-    # set(LIBS ${LIBS} boost_system-mt)
-    # set(LIBS ${LIBS} boost_test_exec_monitor-mt)
-    # set(LIBS ${LIBS} boost_thread-mt)
-    # set(LIBS ${LIBS} boost_timer-mt)
-    # set(LIBS ${LIBS} boost_type_erasure-mt)
-    # set(LIBS ${LIBS} boost_unit_test_framework-mt)
-    # set(LIBS ${LIBS} boost_url-mt)
-    # set(LIBS ${LIBS} boost_wave-mt)
-    # set(LIBS ${LIBS} boost_wserialization-mt)
 
     # winsock2 - работа с сокетами в ОС WINDOWS
     # set(LIBS ${LIBS} ws2_32)
